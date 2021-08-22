5453a3e735954a2af29257c7a1bc1d69166cceb2
Jeff Shen <jshen2014@hotmail.com>
2021-08-22 02:23:15 -0400

    do led roulette

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,23 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, DelayMs, OutputSwitch};
+use volatile::Volatile;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let t = 50_u16;
+
+    loop {
+        for i in 0..8 {
+            leds[i].on().ok();
+            delay.delay_ms(t);
+
+            leds[(i + 1) % 8].on().ok();
+            leds[i].off().ok();
+            delay.delay_ms(t);
+        }
+    }
 }
