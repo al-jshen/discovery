fa1e5753a4d56091fdc7f711165740a2372253be
Jeff Shen <jshen2014@hotmail.com>
2021-08-22 02:23:27 -0400

    do registers

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -2,28 +2,17 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use aux7::{entry, iprint, iprintln};
+use aux7::{entry, iprint, iprintln, ITM};
+use core::ptr;
 
 #[entry]
 fn main() -> ! {
-    aux7::init();
+    let (mut itm, gpioe) = aux7::init();
 
-    unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
-
-        // Turn on the "North" LED (red)
-        *(GPIOE_BSRR as *mut u32) = 1 << 9;
-
-        // Turn on the "East" LED (green)
-        *(GPIOE_BSRR as *mut u32) = 1 << 11;
-
-        // Turn off the "North" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
-
-        // Turn off the "East" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
-    }
+    gpioe.bsrr.write(|w| w.bs9().set_bit());
+    gpioe.bsrr.write(|w| w.bs11().set_bit());
+    gpioe.bsrr.write(|w| w.br9().set_bit());
+    gpioe.bsrr.write(|w| w.br11().set_bit());
 
     loop {}
 }
